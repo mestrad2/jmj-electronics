9f60cd327eb22f6937cf65c1b24837bc694b8fa4
jwatts41 <105758398+jwatts41@users.noreply.github.com>
2022-12-02 11:40:44 -0500

    put in the dependecies and installed sequelize (#3)

---
+++ jmj-electronics/server.js
@@ -0,0 +1,31 @@
+// DEPENDECIES 
+const express = require('express')
+const app = express()
+const { Sequelize } = require('sequelize')
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to JMJ Electronics'
+    })
+})
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+    console.log(`🎮 JMJ Electronics on port: ${process.env.PORT}`)
+})
