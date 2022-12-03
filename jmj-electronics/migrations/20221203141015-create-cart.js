e479d036d98c0086d5f14455ec1d04c2db980e0d
Susan Heller <*jefa8@gmail.com>
2022-12-03 09:16:57 -0500

    added cart

---
+++ jmj-electronics/migrations/20221203141015-create-cart.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Carts', {
+      cart_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      desc: {
+        type: Sequelize.STRING
+      },
+      cost_ea: {
+        type: Sequelize.INTEGER
+      },
+      qty: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1
+      },
+      total: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Carts');
+  }
+};
---
+++ jmj-electronics/models/cart.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Cart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Cart.init({
+    cart_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    desc: {
+      typ3: DataTypes.STRING
+    },
+    cost_ea: {
+      type: DataTypes.INTEGER
+    },
+    qty: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1
+    },
+    total: {
+      type: DataTypes.INTEGER
+    }
+  }, {
+    sequelize,
+    modelName: 'Cart',
+    tableName: 'cart',
+    timestamps: false
+  });
+  return Cart;
+};
