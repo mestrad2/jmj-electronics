5acef5902ce768abe187f5bc6a90151bf2664a0c
Susan Heller <*jefa8@gmail.com>
2022-12-03 13:47:41 -0500

    all routes for appliances_controller added, requires testing

--- controllers/appliances_controller.js
+++ controllers/appliances_controller.js
@@ -13,6 +13,67 @@ appliances.get('/', async (req, res) => {
     }
 })
 
+// FIND A SPECIFIC APPLIANCE, show route
+appliances.get('/:id', async (req, res) => {
+    try {
+        const foundAppliances = await Appliance.findOne({
+            where: { appliances_id: req.params.id }
+        })
+        res.status(200).json(foundAppliances)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// CREATE AN APPLIANCE, create route
+appliances.post('/', async (req, res) => {
+    try {
+        const newAppliance = await Appliance.create(req.body)
+        res.status(200).json({
+            message: 'Successfully inserted a new appliance',
+            data: newAppliance
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// UPDATE AN APPLIANCE, update route
+appliances.put('/:id', async (req, res) => {
+    try {
+        const updatedAppliances = await Appliance.update(req.body, {
+            where: {
+                appliances_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully updated ${updatedAppliances} appliance(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+// DELETE AN APPLIANCE, delete route
+appliances.delete('/:id', async (req, res) => {
+    try {
+        const deletedAppliances = await Appliance.destroy({
+            where: {
+                appliances_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `Successfully deleted ${deletedAppliances} appliance(s)`
+        })
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+
+
+
 
 // EXPORT
 module.exports = appliances
