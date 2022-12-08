883029eb78b90d18eb37011b07081ad9b69e743c
Manuel Estrada <manuel.estrada16@gmail.com>
2022-12-07 20:28:07 -0500

    added fetch for products table in back-end

--- front-end/src/context/Context.js
+++ front-end/src/context/Context.js
@@ -4,8 +4,24 @@ const Cart = createContext()
 
 function Context({ children }) {
 
+    //fetch products from database
+    const products = (e) => {
+        e.preventDefault()
+        fetch('http://localhost:3003/products')
+        .then(response => response.json())
+        .then(resData => {
+            if (resData.results.length > 0) {
+                return resData.results
+            } else {
+                return console.log('Not Found')
+            }
+        })
+        .catch(err => console.log('No Products Available!'))
+    }
+
+    console.log(products)
     return(
-        <Cart.Provider>
+        <Cart.Provider value="">
             {children}
         </Cart.Provider>
     )
